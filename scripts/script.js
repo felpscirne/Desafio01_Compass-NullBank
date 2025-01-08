469ac5a0a643eb6f9a625e30f4a69c0ce8481563
felpscirne <cirnefelipe@gmail.com>
2025-01-08 01:45:55 -0300

    add: first screen

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+
+            var currentPath = window.location.pathname.split('/').pop();
+            var menuItems = document.querySelectorAll('.menu li a');
+
+            menuItems.forEach(function(item) {
+                if (item.getAttribute('href') === currentPath) {
+                    item.classList.add('active');
+                }
+            });
+        });
+});
