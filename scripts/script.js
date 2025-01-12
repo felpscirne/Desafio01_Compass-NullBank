d61a31376abe8e485d827db995a37e5f04f7a46e
felpscirne <cirnefelipe@gmail.com>
2025-01-12 18:42:57 -0300

    add: index structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('../header.html')
-        .then(response => response.text())
+    fetch('../header.html') 
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
 
-            var currentPath = window.location.pathname.split('/').pop();
-            var menuItems = document.querySelectorAll('.menu li a');
+            var menuItems = document.querySelectorAll('.menu li a, .login a.sign-up');
 
+            // Adiciona evento de clique para salvar o ID do botão clicado no localStorage
             menuItems.forEach(function(item) {
-                if (item.getAttribute('href') === currentPath) {
-                    item.classList.add('active');
-                }
+                item.addEventListener('click', function() {
+                    localStorage.setItem('activeButtonId', item.id);
+                });
             });
+
+            // Recupera o ID do botão ativo do localStorage
+            var activeButtonId = localStorage.getItem('activeButtonId');
+
+            // Marca o botão ativo com base no ID armazenado
+            if (activeButtonId) {
+                var activeButton = document.getElementById(activeButtonId);
+                if (activeButton) {
+                    activeButton.style.color = '#6729FF';
+                }
+            }
         })
         .catch(error => console.error('Erro ao carregar o header:', error));
+
+
+
+    fetch('../footer.html') 
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o footer:', error));
+
 });
